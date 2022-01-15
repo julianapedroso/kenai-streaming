e078391d434f1d6323fe963c93b3700148b3d9ed
julianapedroso <juliana.cristina@hotmail.com.br>
2022-01-15 13:34:13 -0300

    :sparkles: folder structure created

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Menu de navegação</h1>
+    </div>
+  );
+};
+
+export default Navbar;
